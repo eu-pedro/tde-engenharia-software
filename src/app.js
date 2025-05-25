2eb34f45abc9067a2e7fcd4d0868a8e655993f9f
pedro henrique <pedro.henrike2812@gmail.com>
2025-05-25 20:52:26 -0300

    feat: criando projeto e adicionando caso de test

---
+++ src/app.js
@@ -0,0 +1,16 @@
+export function appContainer() {
+  const app = document.querySelector('#app')
+  app.innerHTML = `
+    <div>
+      <h1>TDE de Engenharia</h1>
+      <p>Feito por: </p>
+      <span>Pedro Henrique,</span>
+      <span>Carlos Geovane,</span>
+      <span>Luis Gustavo,</span>
+      <span>Gian Carlos,</span>
+      <span> e </span>
+      <span>Adrysson Rhuan</span>
+     
+    </div>
+  `
+}
---
+++ tests/intro.spec.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect, beforeEach, afterEach } from 'vitest'
+import { appContainer } from '../src/app'
+
+describe("App Container", () => {
+  let appDiv; 
+
+  beforeEach(() => {
+    appDiv = document.createElement('div');
+    appDiv.id = 'app';
+    document.body.appendChild(appDiv);
+  });
+
+ 
+
+  it("should render the main title and names inside the #app element", () => {
+    appContainer();
+
+    const app = document.querySelector('#app');
+
+    expect(app).not.toBeNull();
+
+    const h1 = app?.querySelector('h1');
+
+    expect(h1).not.toBeNull(); 
+
+    expect(h1?.textContent).toBe('TDE de Engenharia'); 
+
+    expect(app?.innerHTML).toContain('Pedro Henrique');
+
+    const spans = app?.querySelectorAll('span');
+    
+    expect(spans?.length).toBe(6);
+  });
+});
